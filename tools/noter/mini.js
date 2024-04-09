f46f650ad1b6d94f57fe3c5f46bb545f1eded3d0
computerblade.official <66473206+computerblade-official@users.noreply.github.com>
2024-04-09 12:24:06 +0530

    Update to Version 1.05

--- tools/noter/mini.js
+++ tools/noter/mini.js
@@ -6,3 +6,31 @@ function getUsername() {
         document.getElementById("username").innerHTML = un;
     }
 }
+
+function clearNotesTxt() {
+    let noteTitle = document.getElementById("noteTitle").value;
+    let noteText = document.getElementById("noteText").value;
+    let bgcol = document.getElementById("noteBgColor").value;
+    let fgcol = document.getElementById("notefFgColor").value;
+    noteTitle = "";
+    noteText = "";
+    bgcol = "#000000";
+    fgcol = "#FFFFFF";
+}
+
+function logOutOption() {
+    let un = localStorage.getItem("username");
+    if (un == null || un == "" || un == "user") {
+        document.getElementById("logoutbtn").setAttribute("disabled", "disabled")
+        document.getElementById("notesbtn").setAttribute("disabled", "disabled")
+    } else {
+        document.getElementById("logoutbtn").removeAttribute("disabled")
+        document.getElementById("notesbtn").removeAttribute("disabled")
+    }
+}
+
+function logout() {
+    localStorage.clear();
+    alert("Logged out successfully!")
+    location.reload();
+}
