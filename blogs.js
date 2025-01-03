84e2bef8d41ed8484d3e757a263ef9738a9355ec
computerblade <66473206+computerblade-official@users.noreply.github.com>
2025-01-03 15:48:35 +0530

    new blog

--- blogs.js
+++ blogs.js
@@ -1,14 +1,23 @@
 var blogs = [
   {
-    "title": "Welcome! #2",
-    "datetime": "02-Jan-2025 06:32:52 PM IST",
-    "content": "This is the second ever blog post of this site (copy of the 1st one). Hopefully I manage to write more blogs soon, and yes I will also set up Google Analytics to track how many users visit this site.<br><br>Thanks for reading!",
-    "comments": true
+    title: "Planning to make some changes to the site...",
+    datetime: "03-Jan-2025 03:28:00 PM IST",
+    content:
+      'I am going to add some stuff to the start menu, mainly the \'All Programs\' feature, and also fixing the desktop icons, and the Segoe UI font to match the Windows 7 one. Below is a reference image for you (and yes I do care about minor detail):\r\n<img style="width:100%" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Segoe_UI_Revision_Differences.svg/1920px-Segoe_UI_Revision_Differences.svg.png" alt="reference segoe ui font image">\r\nThanks for reading!',
+    comments: true,
   },
   {
-    "title": "Welcome!",
-    "datetime": "02-Jan-2025 06:32:52 PM IST",
-    "content": "This is the first ever blog post of this site. Hopefully I manage to write more blogs soon, and yes I will also set up Google Analytics to track how many users visit this site.<br><br>Thanks for reading!",
-    "comments": true
-  }
+    title: "Welcome! #2",
+    datetime: "02-Jan-2025 06:32:52 PM IST",
+    content:
+      "This is the second ever blog post of this site (copy of the 1st one). Hopefully I manage to write more blogs soon, and yes I will also set up Google Analytics to track how many users visit this site.<br><br>Thanks for reading!",
+    comments: true,
+  },
+  {
+    title: "Welcome!",
+    datetime: "02-Jan-2025 06:32:52 PM IST",
+    content:
+      "This is the first ever blog post of this site. Hopefully I manage to write more blogs soon, and yes I will also set up Google Analytics to track how many users visit this site.<br><br>Thanks for reading!",
+    comments: true,
+  },
 ];
